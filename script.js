95d1b480231ad9a65e6342497f886d7f8098ead9
Scott M Robertson <141648733+Robertvson@users.noreply.github.com>
2024-12-09 17:37:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Translation Vars
 const translations = {
-    it: {
+    it:{
         aspiring: 'Aspirante sviluppatore web',
         enTeach: 'Insegnante Inglese',
         contactme: 'Contattarmi',
@@ -17,7 +17,57 @@ const translations = {
         eduListI5: "Fondamenti di sicurezza informatica",
         eduListI6: "Bloccare le autorizzazioni per prevenire violazioni secutiy",
         eduListI7: "Fondamenti del HTML e CSS",
+        //Heading and C1 R1 done, below C2 R1
+        workExpTitle: "Esperienza",
+        workExpCosa: "Insegnante di inglese e creatore di contenuti",
+        workExpDiscorso: "In questo ruolo sono stato un insegnante di inglese e un creatore di contenuti. Ho creato contenuti interattivi per i clienti. In questo ruolo ho imparato come utilizzare vari strumenti per creare contenuti grafici. Ho appreso le seguenti abilita':",
+        workExpL1: "Disegno grafico",
+        workExpL2: "Analisi dei dati con Excel",
+        workExpL3: "Base di UX",
+        workExpL4: "Troubleshooting",
+        workExpL5: "Moduli Condizionali",
+        workExpL6: "Archiviazione e organizzazione dei dati in Zoho CRM",
+        workExpL7: "Collaborazione di squadra",
+        //C2 R1 Done, Below TEFL.
+        teflTitle: "Certificato TEFL",
+        teflDiscorso: "Questo certificato mi da' la possibilita' di insegnare l'inglese come lingua straniera. Le competenze che ho acquisito durante il corso sono:",
+        teflL1: "Essere in grado di pianificare e svolgere le lezioni",
+        teflL2: "Grammatica inglese in dettaglio",
+        teflL3: "Organizzazione e pianificazione",
+        teflL4: "Puntualita'",
+        teflL5: "Comunicazione",
+        teflL6: "Principi di insegnamento",
+        teflL7: "Presentare informazioni",
+        //TEFL done, now Egnlish Teacher Freelance
+        eTFTitle: "Insegnante Inglese (Freelance)",
+        eTFDove: "Anglo American School, Preply e NativeCamp",
+        eTFDiscorso: "Ho lavorato come insegnante di inglese per varie istituzioni per un anno, durante questo periodo ho acquisito e migliorato le mie competenze:",
+        eTFL1: "Costruire rapporti con studenti provenienti da contesti diversi, compresi i bambini",
+        eTFL2: "Usare il linguaggio del corpo e le espressioni facciali per superare una barriera linguistica",
+        eTFL3: "Organizzare la mia agenda",
+        eTFL4: "Pianificare in anticipo",
+        eTFL5: "Anticipare malintesi o confusione da argomenti grammaticali",
+        eTFL6: "Migliore comprensione delle culture",
+        //ETF done now Front End Engineer Cert
+        cATitle: "Certificato Ingegneria Font-End",
+        cADove: "CodeAcademy (in corso)",
+        cADiscorso: "Questo corso ha migliorato le mie precedenti competenze in HMTL e CSS e mi ha dato la possibilita' di fare un tentativo di irrompere nel mercato del lavoro attraverso un portfolio ben fatto. Preferisco invece fare i miei pezzi da zero che vedrete in fondo e in questa pagina web. Cose che ho imparato:",
+        cAList1: "Fondamenti di HTML e CSS",
+        cAList2: "Personalizzazione avanzata con CSS",
+        cAList6: "Teoria del colore, accessibilita' e HTML semantico",
+        //cA done now dog grooming, the hardest to translate IMHO
+        dogTitle: "Toelettatore / assistente del toelettatore",
+        dogDiscorso: "Ho lavorato in saloni di grooming per cani per 3 anni come assistente e 6 mesi come toelettatore. Sono qualificato. Ho seguito un corso intensivo di 2,5 mesi che ha coperto tutte le fasi del processo di preparazione. Ho superato il corso e ottenuto il certificato (Open College Network livello 3) Nonostante mi piaccia, non credo che sia la mia vera vocazione. Penso che i cani sono animali domestici migliori dei clienti. Ho imparato e migliorato le seguenti abilita':",
+        dogList1: "Toelettatura dei Cani",
+        dogList2: "Taglio tradizionale e di utilita'",
+        dogList3: "Servizio clienti",
+        dogList4: "Gestione del cane",
+        dogList5: "Comportamento del cane",
+        dogList6: "Controlli sanitari e di benessere dei cani",
+        dogList7: "Mantenere un ambiente pulito, sicuro e igienico per lavorare con gli animali",
+        //Back to english
         translateLink: "Back to English",
+        //Additions
     },
     en: {
         aspiring: 'Aspiring Web Developer',
@@ -34,9 +84,59 @@ const translations = {
         eduListI3: 'Troubleshooting',
         eduListI4: "Ticketing and prioritisation",
         eduListI5: "Cyber security fundamentals",
-        eduListI6: "Locking down permissions to prevent security breaches",
+        eduListI6: "Locking down permissions to prevent secutiy breaches",
         eduListI7: "HTML and CSS fundamentals",
-        translateLink: "Traduci in Italiano",
+        //Heading and C1 R1 done, below C2 R1
+        workExpTitle: "Work Experience",
+        workExpCosa: "English Teacher and Content Creator",
+        workExpDiscorso: "In this role I was an English Teacher and a content creator. I created interactive content for clients. In this role I learned how to use various tools to create graphic content. I learned the following skills:",
+        workExpL1: "Graphic design",
+        workExpL2: "Data analysis with Excel",
+        workExpL3: "Basic UX",
+        workExpL4: "Troubleshooting",
+        workExpL5: "Conditional forms",
+        workExpL6: "Data storage and organisation in Zoho CRM",
+        workExpL7: "Team collaboration",
+        //C2 R1 Done, Below TEFL.
+        teflTitle: "TEFL Certificate",
+        teflDiscorso: "This certificate enables me to teach English as a foreign language. The skills I gained during my time on the course were:",
+        teflL1: "Being able to plan and carry out lessons",
+        teflL2: "English Grammar in detail",
+        teflL3: "Organising and planning ahead",
+        teflL4: "Timekeeping",
+        teflL5: "Communication",
+        teflL6: "Principles of teaching",
+        teflL7: "Presenting information",
+        //TEFL done, now Egnlish Teacher Freelance
+        eTFTitle: "English Teacher (Freelance)",
+        eTFDove: "Anglo American School, Preply and NativeCamp",
+        eTFDiscorso: "I worked as an English Teacher for various institions for a year, during this time I gained and improved my skills in the following areas:",
+        eTFL1: "Building rapport with students from diverse backgrounds, including children",
+        eTFL2: "Using body language and facial expressions to overcome a language barrier",
+        eTFL3: "Organising my schedule",
+        eTFL4: "Plannning ahead",
+        eTFL5: "Anticipating misunderstanding or confusion from grammar topics",
+        eTFL6: "Better cultural understanding",
+        //ETF done now Front End Engineer Cert
+        cATitle: "Front-End Engineer Certificate",
+        cADove: "CodeAcademy (in progress)",
+        cADiscorso: "This course has enhanced my previous skills in HMTL and CSS and has given me the chance to take a shot of breaking into the job market through a well made porfolio. I prefer instead to make my own pieces from scratch which you will see at the bottom and in this webpage. Things I learned:",
+        cAList1: "Fundamentals of HTML and CSS",
+        cAList2: "Enhanced customisation with CSS",
+        cAList6: "Colour theory, Accessibility and Semantic HTML",
+        //cA done now dog grooming, the hardest to translate IMHO
+        dogTitle: "Dog Groomer / Dog Groomer's Assistant",
+        dogDiscorso: "I worked in dog grooming salons for 3 years as an assistant and 6 months as a groomer. I am qualified. I took a 2.5 month intensive course which covered all the stages of the grooming process. I passed the course and got the certificate (Open College Network level 3) Despite enjoying it, I don't think its my true calling. I think dogs make better pets than clients. I learned and improved the following skills:",
+        dogList1: "Dog Grooming",
+        dogList2: "Utility and traditional cuts",
+        dogList3: "Customer service",
+        dogList4: "Dog handling",
+        dogList5: "Dog behaviour",
+        dogList6: "Dog welfare and health checks",
+        dogList7: "To maintain a clean, safe and hygenic enviroment for working with animals",
+        //Back to english
+        translateLink: "Back to English",
+        //Additions
     }
 };
 
